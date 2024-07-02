0407f83e81ec34398d7c45845c11e05abe0bd89c
Bowie <deividson.henrique2016@hotmail.com>
2024-07-02 17:32:11 -0300

    styled_component

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,72 +1,97 @@
 import styles from "./Footer.module.css";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin, FaInstagram, FaFacebookSquare } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import {
+  FooterSection,
+  Container,
+  Col1,
+  Col2,
+  Col3,
+  Col4,
+  StyledLinkCol2,
+  StyledLinkCol3,
+  Maps,
+} from "./styles";
 
 function Footer() {
   return (
-    <section className={styles.footer}>
-      <div className={styles.container}>
-        <div className={styles.col1}>
+    <FooterSection className={styles.footer}>
+      <Container className={styles.container}>
+        <Col1 className={styles.col1}>
           <h2>Software Store</h2>
           <p>Criando Possibilidades</p>
           <p>Para conectar com o mundo!</p>
-        </div>
+        </Col1>
 
-        <div className={styles.col2}>
+        <Col2 className={styles.col2}>
           <h3>Explorar</h3>
 
-          <Link className={styles.link} to="/">
+          <StyledLinkCol2 to="/">
             <p>Home</p>
-          </Link>
-          <Link className={styles.link}>
+          </StyledLinkCol2>
+          <StyledLinkCol2>
             <p>Sobre</p>
-          </Link>
-          <Link className={styles.link} to="/contato">
+          </StyledLinkCol2>
+          <StyledLinkCol2 to="/contato">
             <p>Contato</p>
-          </Link>
-        </div>
+          </StyledLinkCol2>
+        </Col2>
 
-        <div className={styles.col3}>
+        <Col3 className={styles.col3}>
           <h3>Follow</h3>
 
-          <Link className={styles.link} to="https://www.instagram.com/" target="_blank">
+          <StyledLinkCol3
+            className={styles.link}
+            to="https://www.instagram.com/"
+            target="_blank"
+          >
             <p>
               <FaInstagram className={styles.icon} /> Instagram
             </p>
-          </Link>
+          </StyledLinkCol3>
 
-          <Link className={styles.link} to="https://twitter.com/" target="_blank">
+          <StyledLinkCol3
+            className={styles.link}
+            to="https://twitter.com/"
+            target="_blank"
+          >
             <p>
               <FaXTwitter className={styles.icon} /> Twitter
             </p>
-          </Link>
+          </StyledLinkCol3>
 
-          <Link className={styles.link} to="https://www.facebook.com/" target="_blank">
+          <StyledLinkCol3
+            className={styles.link}
+            to="https://www.facebook.com/"
+            target="_blank"
+          >
             <p>
               <FaFacebookSquare className={styles.icon} /> Facebook
             </p>
-          </Link>
+          </StyledLinkCol3>
 
-          <Link className={styles.link} to="https://www.linkedin.com/" target="_blank"> 
+          <StyledLinkCol3
+            className={styles.link}
+            to="https://www.linkedin.com/"
+            target="_blank"
+          >
             <p>
               <FaLinkedin className={styles.icon} /> Linkedin
             </p>
-          </Link>
+          </StyledLinkCol3>
+        </Col3>
 
-        </div>
-
-        <div className={styles.col4}>
-          <iframe
+        <Col4>
+          <Maps
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1974904582203!2d-46.65906922391432!3d-23.56134956159323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa315%3A0xd59f9431f2c9776a!2sAv.%20Paulista%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1717024046274!5m2!1spt-BR!2sbr"
             allowfullscreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
-      </div>
+          ></Maps>
+        </Col4>
+      </Container>
       <h1> &copy; 2024 - Software Store. Todos os direitos reservados.</h1>
-    </section>
+    </FooterSection>
   );
 }
 
---
+++ src/Components/Footer/styles.js
@@ -0,0 +1,221 @@
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+
+const FlexColumn = css`
+  display: flex;
+  align-items: start;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Text = css`
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: solid underline white 1px;
+    text-underline-offset: 3px;
+  }
+
+  @media (max-width: 1280px) {
+    font-size: 0.9rem;
+  }
+`;
+
+export const StyledLinkCol2 = styled(Link)`
+  color: #555;
+  ${Text}
+`;
+
+export const StyledLinkCol3 = styled(Link)`
+  ${Text}
+`;
+
+export const FooterSection = styled.section`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: auto;
+
+  h1 {
+    font-size: 1rem;
+    margin-bottom: 10px;
+  }
+
+  @media (max-width: 1280px) {
+    h1 {
+      font-size: 0.8rem;
+    }
+  }
+
+  @media (max-width: 478px) {
+    h1 {
+      font-size: 0.6rem;
+    }
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 90%;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const Col1 = styled.div`
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 20px;
+  }
+
+  p {
+    margin-block: 0.1rem;
+  }
+
+  ${FlexColumn}
+
+  @media (max-width: 1280px) {
+    h2 {
+      font-size: 1rem;
+    }
+
+    p {
+      font-size: 0.9rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h3 {
+      font-size: 0.9rem;
+    }
+
+    p {
+      font-size: 0.8rem;
+    }
+  }
+
+  @media (max-width: 478px) {
+    p {
+      font-size: 0.7rem;
+    }
+  }
+`;
+
+export const Col2 = styled.div`
+  h3 {
+    font-size: 1.3rem;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-block: 0.2rem;
+    cursor: pointer;
+  }
+
+  ${FlexColumn}
+
+  @media (max-width: 1280px) {
+    h3 {
+      font-size: 1rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h3 {
+      font-size: 0.9rem;
+    }
+
+    p {
+      font-size: 0.8rem;
+    }
+  }
+
+  @media (max-width: 478px) {
+    p {
+      font-size: 0.7rem;
+    }
+  }
+`;
+
+export const Col3 = styled.div`
+  ${FlexColumn}
+  align-items: start;
+  h3 {
+    font-size: 1.3rem;
+    margin-bottom: 10px;
+    width: 100%;
+    text-align: center;
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 5px;
+    color: aqua;
+    cursor: pointer;
+  }
+
+  @media (max-width: 1280px) {
+    h3 {
+      font-size: 1rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h3 {
+      font-size: 0.9rem;
+    }
+
+    p {
+      font-size: 0.8rem;
+    }
+  }
+
+  @media (max-width: 478px) {
+    p {
+      font-size: 0.7rem;
+    }
+  }
+`;
+
+export const Col4 = styled.div``;
+
+export const Maps = styled.iframe`
+  margin-top: 20px;
+  border-radius: 10px;
+  width: 300px;
+  height: 200px;
+  opacity: 0.7;
+  transition: 0.3s all;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  @media (max-width: 1280px) {
+    width: 250px;
+    height: 150px;
+  }
+
+  @media (max-width: 992px) {
+    width: 200px;
+  }
+
+  @media (max-width: 768px) {
+    width: 100px;
+    height: 90px;
+  }
+
+  @media (max-width: 478px) {
+    display: none;
+  }
+`;
